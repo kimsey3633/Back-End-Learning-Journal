cb34b34e6eff2997560828ce5237e76480b2f76a
kimsey3633 <kimsey3633@naver.com>
2024-01-17 14:25:30 +0900

    Java Source code for variable and stuff

---
+++ Java_Learning/src/variable/Var.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var {
+
+    public static void main(String args[]) {
+        String hello = "Hello World!";
+        System.out.println(hello);
+
+        long number = 123456789012L;
+        System.out.println(number);
+    }
+
+}
